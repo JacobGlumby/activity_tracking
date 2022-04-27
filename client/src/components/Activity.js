9640ab848aaf8e8a9baba5a5bcde08ef795d5302
JacobGlumby <Jacobglumby@gmail.com>
2022-04-27 22:48:40 +0200

    💄create activity component

---
+++ client/src/components/Activity.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Activity = ({ title, start, end, time }) => {
+    return (
+        <li>
+           
+                <div className="top-row">
+                    <div className="activity-title">
+                        {title}
+                    </div>
+                    <div className="activity-time">
+                        {time}
+                    </div>
+                </div>
+                <div className="bottom-row">
+                    <div className="activity-start">
+                        Start: {start}
+                    </div>
+                    <div className="activity-end">
+                        End: {end}
+                    </div>
+                </div>
+
+        </li>
+    );
+};
+
+export default Activity;
