573281bc31f0ced7e7af22d7509a356395a37989
JacobGlumby <Jacobglumby@gmail.com>
2022-04-27 22:49:09 +0200

    💄fetch and map activities

--- client/src/components/ActivityList.js
+++ client/src/components/ActivityList.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import Activity from "./Activity";
+
 
 const ActivityList = () => {
-    return(
+
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('/activities')
+            .then(res => {
+                setActivities(res.data.activities);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+    console.log(activities)
+    return (
         <div className="activity-container">
-            <ul className="activity-list"></ul>
+            <ul className="activity-list">
+                {
+                    activities.map((activity) =>
+                        <Activity key={activity.id} title={activity.title} start={activity.start} end={activity.end} time={activity.time} />
+                    )
+                }
+            </ul>
         </div>
     );
-}
+};
 
 export default ActivityList;
