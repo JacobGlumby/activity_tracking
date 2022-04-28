83f45ad727458cb99d3fc02ad0cd2edd5650fe7c
JacobGlumby <Jacobglumby@gmail.com>
2022-04-28 21:53:33 +0200

    🥅 Make application ready for error handling

--- server/server.js
+++ server/server.js
@@ -1,39 +1,29 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const db = require("./db/activities");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.post("/activities", async (req,res) => {
-    const results = await db.createActivity(req.body);
-    res.status(201).json({id: results[0]});
+app.post("/activities", async (req, res) => {
+    try {
+        const results = await db.createActivity(req.body);
+        res.status(201).json({ id: results[0] });
+    } catch (error) {
+        res.status(500).json({error: error})
+        //TODO: some sofisticated error handeling
+    }
 })
 
 app.get("/activities", async (req, res) => {
+    try{
     const activities = await db.getAllActivities();
-    res.status(200).json({activities});
+    res.status(200).json({ activities });
+    } catch (error) {
+        res.status(500).json({error: error})
+        //TODO: some sofisticated error handeling
+    }
 })
 
-app.get("/api", (req, res) => {
-    res.json(
-        {
-        "activities": [
-            {
-                "ID": 1,
-                "title": "Writing code",
-                "start" :  "Tue Apr 26 2022 20:00:15 GMT+0200 (Centraleuropæisk sommertid)",
-                "end":  "Tue Apr 26 2022 20:06:15 GMT+0200 (Centraleuropæisk sommertid)"
-            },
-            {
-                "ID": 2,
-                "title": "Writing code again",
-                "start" :  "Tue Apr 26 2022 20:07:15 GMT+0200 (Centraleuropæisk sommertid)",
-                "end":  "Tue Apr 26 2022 20:10:15 GMT+0200 (Centraleuropæisk sommertid)"
-            },
-        ]
-    })
-})
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.listen(5000, () => { console.log("Server started on port 5000") })
