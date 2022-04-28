b72f2e85e436bf7ffd013ab237cb2af7e1aa8948
JacobGlumby <Jacobglumby@gmail.com>
2022-04-28 21:50:33 +0200

    ♻️ refactor from Math.floor to Math.round
    Gives more precise time difference between start and end time.

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,15 +4,15 @@ function pad(time) {
   return time.toString().padStart(2, '0');
 }
 
-function timeDifference(dateBegin,dateEnd){
-  const diffMs = dateBegin- dateEnd;
-  const diffSec = Math.floor(diffMs / 1000 % 60);
-  const diffMin = Math.floor(diffMs / 1000 / 60 % 60);
-  const diffHr = Math.floor(diffMs / 1000 / 60 / 60);
+function calculateTimeDifference(dateBegin,dateEnd){
+  const diffMs = Math.abs(dateBegin- dateEnd);
+  const diffSec = Math.round(diffMs / 1000 % 60);
+  const diffMin = Math.round(diffMs / 1000 / 60 % 60);
+  const diffHr = Math.round(diffMs / 1000 / 60 / 60);
   return `${pad(diffHr)}:${pad(diffMin)}:${pad(diffSec)}`
 }
 
 module.exports = {
   pad,
-  timeDifference
+  calculateTimeDifference
 }
