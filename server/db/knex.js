56943eb7469e8de607578e480d60a7a1f47d2bb9
JacobGlumby <Jacobglumby@gmail.com>
2022-04-28 21:51:05 +0200

    💡Comment on knex module

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,5 +1,10 @@
 const knex = require("knex");
 
+/*
+Database agnostic module that serves as an abstraction layer between the application and the database 
+Eliminates the need to take actions in order to prevent sql injections.
+*/
+
 const connectedKnex = knex({
     client: "sqlite3",
     connection: {
